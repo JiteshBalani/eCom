dff76eb07d06089d827364b17f7a6121b34d068b
JiteshBalani <balani.jitesh911@gmail.com>
2025-05-28 14:31:00 +0530

    Minor UI changes

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/clerk-react'
 const Login = () => {
   return (
     <div className='bg-[#4F39F6] min-h-screen flex items-center justify-center'>
-      <div className='w-full max-w-md'>
+      <div className='w-full max-w-md mx-20'>
         <SignIn signUpUrl='/signup' forceRedirectUrl={'/'}/>
       </div>
     </div>
--- client/src/pages/SignUp/Signup.jsx
+++ client/src/pages/SignUp/Signup.jsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/clerk-react'
 const Signup = () => {
   return (
     <div className='bg-[#4F39F6] min-h-screen flex items-center justify-center'>
-        <div className='w-full max-w-md'>
+        <div className='w-full max-w-md mx-20'>
             <SignUp signInUrl='/login' forceRedirectUrl={'/'}/>
         </div>
     </div>
